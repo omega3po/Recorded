3b3b67844be1805960874698007cde7180b07e01
Sunwoo <sunnyhwang0621@gmail.com>
2023-11-13 20:35:06 -0600

    Initial Commit

---
+++ Recorded/RecordedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordedApp.swift
+//  Recorded
+//
+//  Created by Sunny Hwang on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
